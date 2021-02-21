18577c5e9a8d182d6e3709ff3d6e549295fd5d85
YiChun Sung <ycsung.r@gmail.com>
2021-02-22 00:19:33 +0800

    Added electron dist

--- main.js
+++ main.js
@@ -25,11 +25,11 @@ function createWindow() {
     win = new BrowserWindow({
         width: 1200,
         height: 700,
-        maximizable: false
+        maximizable: true
     })
 
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, './dist/fabulous-day/index.html'),
         protocol: 'file:',
         slashes: true
     }))
